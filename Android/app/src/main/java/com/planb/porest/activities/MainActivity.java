e3219ef7c47a658212add2b73931d2cf7de65d73
JoMingyu <city7310@naver.com>
2017-07-22 22:23:40 +0900

    Main toolbar, ready to recyclerview

--- Android/app/src/main/java/com/planb/porest/activities/MainActivity.java
+++ Android/app/src/main/java/com/planb/porest/activities/MainActivity.java
@@ -1,14 +1,71 @@
 package com.planb.porest.activities;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.planb.porest.R;
 import com.planb.porest.activities.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+    }
+}
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+    String itm;
+
+    public RecyclerAdapter(String itm){
+        this.itm = itm;
+    }
+
+    // 새로운 뷰 홀더 생성
+    @Override
+    public RecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_tree, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // View 의 내용을 해당 포지션의 데이터로 바꿉니다.
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mNameTv.setText(mItems.get(position).getName());
+    }
+
+    // 데이터 셋의 크기를 리턴해줍니다.
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    // 커스텀 뷰홀더
+// item layout 에 존재하는 위젯들을 바인딩합니다.
+    class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView title;
+        private TextView leaf;
+        private TextView date;
+        private TextView shared;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.title);
+            leaf = (TextView) itemView.findViewById(R.id.leaf);
+            date = (TextView) itemView.findViewById(R.id.date);
+            shared = (TextView) itemView.findViewById(R.id.shared);
+        }
     }
 }
--- Android/app/src/main/java/com/planb/porest/activities/Signup.java
+++ Android/app/src/main/java/com/planb/porest/activities/Signup.java
@@ -2,7 +2,6 @@ package com.planb.porest.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
