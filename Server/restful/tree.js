95439c1528819fbe494e8562ebc53347114de390
JoMingyu <city7310@naver.com>
2017-07-22 15:04:51 +0900

    오후 3시 4분 아이디어 수정안 적용 덤프

--- Server/restful/tree.js
+++ Server/restful/tree.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router.route('/tree').post((req, res) => {
     // 나무 만들기
+    let id = req.body.id;
+    let maximumLeaves = req.body.maximum_leaves;
 }).get((req, res) => {
     // 나무 리스트
 });
