9e0dcb3f621538a0eb384ac4e959d24350e90d61
JoMingyu <city7310@naver.com>
2017-07-22 22:23:12 +0900

    Tree VO

---
+++ Android/app/src/main/java/com/planb/porest/support/vo/Tree.java
@@ -0,0 +1,12 @@
+package com.planb.porest.support.vo;
+
+/**
+ * Created by dsm2016 on 2017-07-22.
+ */
+
+public class Tree {
+    public String title;
+    public String leaf;
+    public String date;
+    public String shared;
+}
